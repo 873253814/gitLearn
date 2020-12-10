3698685ea5ebda9a8634c2644b0f1ef5aab8fe6d
sqjg <873253814@qq.com>
2020-12-10 10:58:04 +0800

    Git Learn Test1

---
+++ src/main/java/threadtest/ThreadMain.java
@@ -0,0 +1,9 @@
+package threadtest;
+
+public class ThreadMain {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test1");
+        System.out.println("Test");
+    }
+}
