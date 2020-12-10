5332e56bbeb2b7dadf46e371b03833afb12dfffd
sqjg <873253814@qq.com>
2020-12-10 10:59:26 +0800

    Git Learn Test Delete

--- src/main/java/threadtest/ThreadMain.java
+++ src/main/java/threadtest/ThreadMain.java
@@ -2,8 +2,6 @@ package threadtest;
 
 public class ThreadMain {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test1");
-        System.out.println("Test");
+
     }
 }
